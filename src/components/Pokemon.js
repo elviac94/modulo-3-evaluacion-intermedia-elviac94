5a014b6091fb4ea692c790af8603adcbd55ab8fa
Elvira <elvi_ac94@hotmail.com>
2020-04-07 10:13:46 +0200

    last commit

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 const Pokemon = (props) => {
     return (
-        <li>
+        <li className="pokemons___container">
             <img src={props.image} alt={props.name} />
             <h1>{props.name}</h1>
-            <h2>{props.types}</h2>
+            <ul>{props.types.map((singleType, index) =>
+                <li key={index}>
+                    {singleType}
+                </li>)}
+            </ul>
+
         </li>
     )
 }
